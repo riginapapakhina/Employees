ef5991a6ed00bdaba8587e6d881c9f06155858c4
Rigina <papakhina.rigina@yandex.ru>
2022-06-15 14:35:25 +0300

    classes to React

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,17 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+const data =[
+  {name:"Jane Black", salary:800, increase: false, id: 1},
+  {name:"Timothee Barns", salary:3000, increase: false, id: 2},
+  {name:"Ben Stiller", salary:1230, increase: true, id: 3},
+  {name:"Hue Pills", salary:1240, increase: false, id: 4},
+  {name:"Tanya Marbles", salary:7000, increase: false, id: 5},
+  {name:"Rigina Papakhina", salary:1400, increase: false, id: 6},
+  {name:"Arina Romashkina", salary:1500, increase: false, id: 7}
+]
+
   return (
     <div className="app">
         <AppInfo />
@@ -16,7 +27,7 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList/>
+        <EmployeesList data={data}/>
         <EmployeesAddForm/>
     </div>
   );
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,13 +1,37 @@
+import { Component } from 'react';
+
 import './employees-list-item.css';
 
-const EmployeesListItem = () => {
+class EmployeesListItem extends Component {
+constructor(props){
+  super(props);
+  this.state={
+    increase: false
+  }
+}
+
+onIncrease =()=> {
+  this.setState(({increase})=>({
+    increase: !increase
+  }))
+}
+render(){
+const{name,salary}= this.props;
+const{increase}=this.state;
+
+let classNames = "list-group-item d-flex justify-content-between";
+if(increase){
+  classNames += ' increase'
+}
+
     return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Smith</span>
-            <input type="text" className="list-group-item-input" defaultValue="1000$"/>
+        <li className={classNames}>
+            <span className="list-group-item-label">{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary +"$"}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
-                    className="btn-cookie btn-sm ">
+                    className="btn-cookie btn-sm "
+                    onClick={this.onIncrease}>
                     <i className="fas fa-cookie"></i>
                 </button>
 
@@ -19,6 +43,7 @@ const EmployeesListItem = () => {
             </div>
         </li>
     )
+}
 }
 
 export default EmployeesListItem;
