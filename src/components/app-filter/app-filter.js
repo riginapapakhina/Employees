e5dc0e46a4d86b3ba1afc14bca1dc74d8d6d3cd5
Rigina <papakhina.rigina@yandex.ru>
2022-06-21 11:35:26 +0300

    final

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -4,7 +4,7 @@ const AppFilter = (props) => {
   const buttonsData=[
     {name: 'all', label: 'Все сотрудники'},
     {name: 'rise', label: 'На повышение'},
-    {name: 'moreThan1000', label: 'З/П больше 1000$'},
+    {name: 'moreThan1000', label: 'З/П больше 1000$'}
   ];
   const buttons = buttonsData.map(({name, label})=>{
     const active =props.filter=== name
--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,7 +3,7 @@ import "./app-info.css";
 const AppInfo = ({increased, employees}) => {
     return (
         <div className="app-info">
-            <h1>Учет сотрудников в компании ThisName</h1>
+            <h1>Учет сотрудников в компании The Company</h1>
             <h2>Общее число сотрудников: {employees}</h2>
             <h2>Премию получат: {increased}</h2>
         </div>
